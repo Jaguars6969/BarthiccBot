663094f4356d6926b1409f058067edb4d7e22824
Sarthak P P <sps1695@g.coppellisd.com>
2020-11-27 00:38:00 +0000

    Fixed timer

--- main.py
+++ main.py
@@ -438,16 +438,18 @@ async def dm(ctx, person:discord.Member, *message):
 	time.sleep(2)
 tim = 0
 timeCreator = ""
-client.command()
-async def timer(ctx, hours, minutes, second):
+@client.command()
+async def timer(ctx, hours:int, minutes:int, second:int):
 	
 
-	if hours > 10 and minutes > 120 and  second > 200:
+	if hours < 10 and minutes < 120 and  second < 200:
 		tim = second + minutes * 60 + hours *3600
 		timeCreator = ctx.author
+		
 
 	await ctx.send(embed=discord.Embed(title = "**__Timer__** :timer:", color = random.choice(color), description = f"{timeCreator.mention} set a timer for {hours} hours, {minutes} minutes, and {second} seconds"))
-	await asyncio.time(tim)
+	await asyncio.sleep(tim)
+	await ctx.send(embed=discord.Embed(title = "**__Timer__** :timer:", color = random.choice(color), description = f"{timeCreator.mention}'s timer is done"))
 	
 
 
